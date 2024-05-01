3ae4a3eb88a15cdcd6e689d07ab670b719b3bfe4
Engielolz <109139281+Engielolz@users.noreply.github.com>
2024-04-30 22:40:33 -0700

    Scoreboard criteria support in MakeINF (no support in the inf parsers yet) Add support for moving in MakeINF

--- tools/makeinf.cmd
+++ tools/makeinf.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 if exist "%1" set filename=%1 && goto load
-set /p filename=INF file:
+set /p filename=INF file: 
 if exist "%filename%" goto load
 set current=1
 set last=1
@@ -22,28 +22,30 @@ echo 1. Friendly Name: !f%current%name!
 echo 2. File path: !f%current%fp!
 echo 3. File name: !f%current%fn!
 echo 4. Scoreboard name: !f%current%sc!
-echo 5. HP Recovered: !f%current%hp!
-if !f%current%vuln! == 1 echo 6. [X] Requires plantable food exploit protection
-if !f%current%vuln! == 0 echo 6. [ ] Requires plantable food exploit protection
-echo X. Delete this food
+echo 5. Scoreboard criteria: !f%current%cc!
+echo 6. HP Recovered: !f%current%hp!
+if !f%current%vuln! == 1 echo 7. [X] Requires plantable food exploit protection
+if !f%current%vuln! == 0 echo 7. [ ] Requires plantable food exploit protection
+echo X. Delete/move this food
 echo.
-echo 7. Switch food
+echo S. Switch food
 echo 8. Change global directory (currently %working%/%dir%)
 echo 9. Write changes
 
 echo 0. Exit
-choice /c:123456X7890
+choice /c:1234567XS890
 if %errorlevel% == 1 goto newfood
 if %errorlevel% == 2 goto filepath
 if %errorlevel% == 3 goto filename
 if %errorlevel% == 4 goto scoreboard
-if %errorlevel% == 5 goto hpval
-if %errorlevel% == 6 goto toggle
-if %errorlevel% == 7 goto delete
-if %errorlevel% == 8 goto listfood
-if %errorlevel% == 9 goto editdir
-if %errorlevel% == 10 goto write
-if %errorlevel% == 11 goto confirmexit
+if %errorlevel% == 5 goto criteria
+if %errorlevel% == 6 goto hpval
+if %errorlevel% == 7 goto toggle
+if %errorlevel% == 8 goto movedel
+if %errorlevel% == 9 goto listfood
+if %errorlevel% == 10 goto editdir
+if %errorlevel% == 11 goto write
+if %errorlevel% == 12 goto confirmexit
 
 :newfood
 set /p f%current%name=Friendly Name: 
@@ -61,6 +63,10 @@ goto menu
 :scoreboard
 echo Reminder: This must be 8 characters or less
 set /p f%current%sc=Scoreboard name: 
+goto menu
+
+:criteria
+set /p f%current%cc=Enter scoreboard criteria: 
 goto menu
 
 :hpval
@@ -90,6 +96,7 @@ goto listloop
 :switch
 set current=%myvar%
 set myvar=
+if "%1" == "--move" exit /b
 set /p current=Switch to food (currently %current%): 
 if not %current% GTR %last% goto menu
 set /a last=%last% + 1
@@ -123,19 +130,47 @@ set /p working=Enter new working directory:
 set "working=%working:\=/%"
 goto menu
 
+:movedel
+echo Do what with !f%current%name!?
+echo 1. Move
+echo 2. Delete
+echo 3. Cancel
+choice /c:123
+if %errorlevel% == 2 goto delete
+if %errorlevel% == 3 goto menu
+call :listfood --move
+echo The current food is !f%current%name! in slot %current%.
+set /p myvar=Switch with what slot? 
+if %myvar% == %current% goto menu
+REM Backup the current food into move
+set nextup=%current%
+set current=move
+call :delloop --move
+REM Copy Food 2 into Food 1
+set current=%nextup%
+set nextup=%myvar%
+call :delloop --move
+REM Copy move into Food 2
+set nextup=move
+set current=%myvar%
+call :delloop --move
+goto menu
 :delete
-echo You asked to delete !f%current%name! Are you sure?
+echo You asked to delete !f%current%name!. Are you sure?
 choice /c:yn
 if %errorlevel% == 2 goto menu
 if %current% == %last% goto stopdel
 set /a nextup=%current% + 1
 :delloop
+echo Copying !f%current%name! TO !f%nextup%name!
 set f%current%name=!f%nextup%name!
 set f%current%fp=!f%nextup%fp!
 set f%current%fn=!f%nextup%fn!
 set f%current%sc=!f%nextup%sc!
+set f%current%cc=!f%nextup%cc!
 set f%current%hp=!f%nextup%hp!
 set f%current%vuln=!f%nextup%vuln!
+if "%1" == "--move" exit /b
 if %nextup% == %last% goto stopdel
 set /a nextup=%nextup% + 1
 set /a current=%current% + 1
@@ -147,6 +182,7 @@ set f%current%name=
 set f%current%fp=
 set f%current%fn=
 set f%current%sc=
+set f%current%cc=
 set f%current%hp=
 set f%current%vuln=
 set /a last=%last% - 1
@@ -169,6 +205,7 @@ echo f%current%name=!f%current%name!
 echo f%current%fp=!f%current%fp!
 echo f%current%fn=!f%current%fn!
 echo f%current%sc=!f%current%sc!
+echo f%current%cc=!f%current%cc!
 echo f%current%hp=!f%current%hp!
 echo f%current%vuln=!f%current%vuln!
 )
@@ -205,7 +242,7 @@ exit /b
 :error
 echo Failed to save the file %filename%...
 pause
-exit /b
+goto menu
 
 :badsignature
 echo Bad signature - %Signature%
